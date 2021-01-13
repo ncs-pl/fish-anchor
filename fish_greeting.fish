d9d1b97ead0612b345753a4bca6f5dcb630f630d
n1c00o <git.n1c00o@gmail.com>
2021-01-13 22:13:53 +0100

    feat(fish_greeting): add custom greeting

---
+++ fish_greeting.fish
@@ -0,0 +1,8 @@
+# Anchor - Elegant and minimalist theme for the fish shell
+# https://github.com/n1c00o/fish-anchor
+
+function fish_greeting
+  if type -q fish_logo
+    fish_logo blue cyan red
+  end
+end
